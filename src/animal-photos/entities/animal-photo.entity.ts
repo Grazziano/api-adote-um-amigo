cc6b861404bebfa481af77ff867ef67cdf53d0b7
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-08-22 12:26:17 -0300

    feat: creating animal_photo entity

--- src/animal-photos/entities/animal-photo.entity.ts
+++ src/animal-photos/entities/animal-photo.entity.ts
@@ -1,1 +1,21 @@
-export class AnimalPhoto {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('animal_photos')
+export class AnimalPhoto {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'text' })
+  photo_url: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
