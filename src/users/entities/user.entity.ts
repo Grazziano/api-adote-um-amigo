f63213ecfd89682500bf115b1193f48a7eccd094
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-08-22 10:59:08 -0300

    feat: creating user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,36 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 150 })
+  name: string;
+
+  @Column({ unique: true, length: 50 })
+  email: string;
+
+  @Column()
+  password_hash: string;
+
+  @Column({ nullable: true, length: 20 })
+  phone: string;
+
+  @Column({ nullable: true, type: 'text' })
+  address: string;
+
+  @Column({ type: 'enum', enum: ['user', 'admin'], default: 'user' })
+  user_type: 'user' | 'admin';
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
