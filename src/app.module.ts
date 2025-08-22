98bf0d855f3ec73da5a79bff125d02a63b1fc733
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-08-22 11:01:58 -0300

    feat: generating structure for animal crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AnimalsModule } from './animals/animals.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AnimalsModule],
   controllers: [AppController],
   providers: [AppService],
 })
