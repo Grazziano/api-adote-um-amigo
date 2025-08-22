352db6196e3b965cd761527c27c420b4444ec3ee
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-08-22 11:12:02 -0300

    feat: creating animal entity

--- src/animals/entities/animal.entity.ts
+++ src/animals/entities/animal.entity.ts
@@ -1,1 +1,42 @@
-export class Animal {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('animals')
+export class Animal {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 150 })
+  name: string;
+
+  @Column({ type: 'enum', enum: ['dog', 'cat', 'other'] })
+  species: 'dog' | 'cat' | 'other';
+
+  @Column({ nullable: true })
+  breed: string;
+
+  @Column({ nullable: true })
+  age: number;
+
+  @Column({ type: 'enum', enum: ['male', 'female'] })
+  sex: 'male' | 'female';
+
+  @Column({ type: 'enum', enum: ['small', 'medium', 'large'] })
+  size: 'small' | 'medium' | 'large';
+
+  @Column({ nullable: true, type: 'text' })
+  description: string;
+
+  @Column({ type: 'enum', enum: ['available', 'adopted', 'in progress'] })
+  status: 'available' | 'adopted' | 'in progress';
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
