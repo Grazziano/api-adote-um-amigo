59a7e6f575461de8a4572a688e75f2e53f326d4c
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-08-23 10:41:52 -0300

    feat: implementing create animal dto

--- src/animals/dto/create-animal.dto.ts
+++ src/animals/dto/create-animal.dto.ts
@@ -1,1 +1,44 @@
-export class CreateAnimalDto {}
+import {
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsString,
+  IsUUID,
+  Length,
+  Min,
+} from 'class-validator';
+
+export class CreateAnimalDto {
+  @IsString()
+  @Length(2, 150)
+  name: string;
+
+  @IsEnum(['dog', 'cat', 'other'])
+  species: 'dog' | 'cat' | 'other';
+
+  @IsOptional()
+  @IsString()
+  breed?: string;
+
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  age?: number;
+
+  @IsEnum(['male', 'female'])
+  sex: 'male' | 'female';
+
+  @IsEnum(['small', 'medium', 'large'])
+  size: 'small' | 'medium' | 'large';
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsEnum(['available', 'adopted', 'in progress'])
+  status?: 'available' | 'adopted' | 'in progress';
+
+  @IsUUID()
+  ong_id: string;
+}
