eaa8186aa91665c22d9c7b1c736ee23f95ca1ccd
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-08-23 10:28:16 -0300

    feat: implementing user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsOptional, IsString, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @Length(2, 150)
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsEnum(['user', 'admin'])
+  user_type?: 'user' | 'admin';
+}
