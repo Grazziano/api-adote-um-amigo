3f0caadffbb33412b7d013140a8a16cdfbb04b1f
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-08-23 10:28:39 -0300

    feat: implementing update user dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,30 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsEnum, IsOptional, IsString, Length } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
+  @IsString()
+  @Length(2, 150)
+  name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  password?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsEnum(['user', 'admin'])
+  user_type?: 'user' | 'admin';
+}
