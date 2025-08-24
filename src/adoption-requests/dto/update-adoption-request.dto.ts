f143de4be5d16fcb79e4d28cd1c5a1caef5ccf7d
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-08-24 09:27:43 -0300

    feat: implementing UpdateAdoptionRequestDto

--- src/adoption-requests/dto/update-adoption-request.dto.ts
+++ src/adoption-requests/dto/update-adoption-request.dto.ts
@@ -1,6 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAdoptionRequestDto } from './create-adoption-request.dto';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class UpdateAdoptionRequestDto extends PartialType(
   CreateAdoptionRequestDto,
-) {}
+) {
+  @IsOptional()
+  @IsEnum(['pending', 'approved', 'rejected'])
+  status?: 'pending' | 'approved' | 'rejected';
+
+  @IsOptional()
+  @IsString()
+  message?: string;
+}
