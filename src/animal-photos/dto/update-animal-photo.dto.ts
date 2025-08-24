78babf64f65427aa36ff4d2e552b50888ec54796
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-08-24 09:18:48 -0300

    feat: implementing UpdateAnimalPhotoDto

--- src/animal-photos/dto/update-animal-photo.dto.ts
+++ src/animal-photos/dto/update-animal-photo.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAnimalPhotoDto } from './create-animal-photo.dto';
+import { IsUrl, IsUUID } from 'class-validator';
 
-export class UpdateAnimalPhotoDto extends PartialType(CreateAnimalPhotoDto) {}
+export class UpdateAnimalPhotoDto extends PartialType(CreateAnimalPhotoDto) {
+  @IsUUID()
+  animalId?: string;
+
+  @IsUrl()
+  photoUrl?: string;
+}
