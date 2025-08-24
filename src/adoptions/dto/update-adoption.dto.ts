dbfd35a56416a6c25af18b2b6969cca43e1f954d
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-08-24 09:31:21 -0300

    feat: implementing update adoption dto

--- src/adoptions/dto/update-adoption.dto.ts
+++ src/adoptions/dto/update-adoption.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAdoptionDto } from './create-adoption.dto';
+import { IsUUID } from 'class-validator';
 
-export class UpdateAdoptionDto extends PartialType(CreateAdoptionDto) {}
+export class UpdateAdoptionDto extends PartialType(CreateAdoptionDto) {
+  @IsUUID()
+  animalId?: string;
+
+  @IsUUID()
+  adopterId?: string;
+}
