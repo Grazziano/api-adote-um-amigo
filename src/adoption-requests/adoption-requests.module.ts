bad4caae64ca03841392c0517e02719944749e87
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-08-26 10:12:39 -0300

    feat: creating the create method of the adoption requests service

--- src/adoption-requests/adoption-requests.module.ts
+++ src/adoption-requests/adoption-requests.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AdoptionRequestsService } from './adoption-requests.service';
 import { AdoptionRequestsController } from './adoption-requests.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdoptionRequest } from './entities/adoption-request.entity';
+import { Animal } from 'src/animals/entities/animal.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AdoptionRequest, Animal, User])],
   controllers: [AdoptionRequestsController],
   providers: [AdoptionRequestsService],
 })
--- src/adoption-requests/adoption-requests.service.ts
+++ src/adoption-requests/adoption-requests.service.ts
@@ -1,11 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAdoptionRequestDto } from './dto/create-adoption-request.dto';
 import { UpdateAdoptionRequestDto } from './dto/update-adoption-request.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AdoptionRequest } from './entities/adoption-request.entity';
+import { Animal } from 'src/animals/entities/animal.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class AdoptionRequestsService {
-  create(createAdoptionRequestDto: CreateAdoptionRequestDto) {
-    return 'This action adds a new adoptionRequest';
+  constructor(
+    @InjectRepository(AdoptionRequest)
+    private readonly adoptionRequestsRepository: Repository<AdoptionRequest>,
+
+    @InjectRepository(Animal)
+    private readonly animalsRepository: Repository<Animal>,
+
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create(createAdoptionRequestDto: CreateAdoptionRequestDto) {
+    const animal = await this.animalsRepository.findOne({
+      where: { id: createAdoptionRequestDto.animalId },
+    });
+
+    if (!animal) {
+      throw new NotFoundException(
+        `Animal ${createAdoptionRequestDto.animalId} não encontrado`,
+      );
+    }
+
+    const adopter = await this.usersRepository.findOne({
+      where: { id: createAdoptionRequestDto.adopterId },
+    });
+
+    if (!adopter) {
+      throw new NotFoundException(
+        `Adopter ${createAdoptionRequestDto.adopterId} não encontrado`,
+      );
+    }
+
+    const request = this.adoptionRequestsRepository.create({
+      ...createAdoptionRequestDto,
+      animal,
+      adopter,
+    });
+
+    return this.adoptionRequestsRepository.save(request);
   }
 
   findAll() {
